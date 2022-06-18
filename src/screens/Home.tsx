168c646bc5c3aa60339e1c3c96f9f14361b129d4
amritatwal <amritatwal04@gmail.com>
2022-06-18 15:44:46 +0100

    Updated logic for getUsersChatData()
    Able to iterate through each chat id and retrieve contact information, however needs to be re-written in order to account for promises

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,12 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button, Text, View, TextInput, StyleSheet } from 'react-native'
 import { RootStackParamList } from '../../types';
 import { auth } from '../firebase/config';
-import { getDatabase, ref, set, onValue, child, get } from 'firebase/database';
+import { getDatabase, ref, set, push, child, get, update } from 'firebase/database';
 import { v4 as uuidv4 } from 'uuid';
+import { database } from 'react-native-firebase';
 
 
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
@@ -14,7 +15,8 @@ type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 export const Home: React.FC<HomeScreenProps> = (props) => {
   // Initialize Cloud Firestore and get a reference to the service
   const db = getDatabase();
-  const [contact, setContact] = useState<string>("");
+  const [input, setInput] = useState<string>("");
+  const [chats, setChats] = useState();
 
   /*
   TODO:
@@ -28,7 +30,7 @@ export const Home: React.FC<HomeScreenProps> = (props) => {
   function handleNewChat() {
     // UI: pop-up/modal? user is prompted to either add a new contact or create a new group (after MVP)
     // Adding a new contact to create a chat entails: retrieving the contact's name (in order to provide a name when creating/displaying a user's contact list), a phone number or email (MVP)...
-    function writeChatData(currentUser: string, contact: string) {
+    function writeChatData(currentUser: string, input: string) {
       const chatUUID: string = uuidv4().toString();
       console.log(currentUser)
       // TODO: 
@@ -38,13 +40,15 @@ export const Home: React.FC<HomeScreenProps> = (props) => {
           lastMessage: false,
           timestamp: false
         });
-        set(ref(db, 'users/' + currentUser + '/chats'), {
+
+        const usersChatRef = ref(db, 'users/' + currentUser + '/chats');
+        update(usersChatRef, {
           [chatUUID]: true
-        }
-        )
+        })
+
         set(ref(db, 'members/' + chatUUID), {
           [currentUser]: true,
-          [contact]: true
+          [input]: true
         })
       } catch (error: unknown) {
         if (error instanceof Error) {
@@ -52,7 +56,7 @@ export const Home: React.FC<HomeScreenProps> = (props) => {
         }
       }
     }
-    writeChatData(auth.currentUser.uid, contact);
+    writeChatData(auth.currentUser.uid, input);
   }
 
 
@@ -64,36 +68,47 @@ export const Home: React.FC<HomeScreenProps> = (props) => {
   // Read in user's active chats from realtime database
   function getUsersChatIDs(currentUser: string | null) {
     // Locate the chat ids under user's uid
-    const contactsList = ref(db, 'users/' + currentUser + '/chats');
-    onValue(contactsList, (snapshot) => {
-      const data = snapshot.val();
-      const chats: string[] = Object.keys(data);
-      console.log(chats)
-      getUsersChatData(chats)
-    })
+    const dbRef = ref(db);
+    get(child(dbRef, 'users/' + currentUser + '/chats')).then((snapshot) => {
+      if (snapshot.exists()) {
+        const data: string[] = (Object.keys(snapshot.val()));
+        console.log(data)
+        getUsersChatData(data);
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
   }
 
   // Use the chat uids to retrieve data from /chats 
 
-  function getUsersChatData(chats: string[] | null) {
+  function getUsersChatData(listOfIds: string[] | null) {
+    const data:any = [];
     const dbRef = ref(db);
-    if (chats) {
-      const results = chats.map(id => {
+    if (listOfIds) {
+      listOfIds.forEach(id => {
         get(child(dbRef, `members/${id}`)).then((snapshot) => {
-          if (snapshot.exists()) {
-            console.log(snapshot.val());
-          } else {
-            console.log("No data available");
-          }
-        }).catch((error) => {
-          console.error(error);
-        });
+          snapshot.forEach(childSnapshot => {
+            if (childSnapshot.key !== auth.currentUser.uid) {
+              data.push(childSnapshot.key)
+            }
+          })
+        })
       })
+    }
+    if (data) {
+      console.log(data)
+      setChats(data);
+      console.log(chats);
     }
   }
 
 
-  getUsersChatIDs(auth.currentUser.uid);
+  useEffect(() => {
+    getUsersChatIDs(auth.currentUser.uid);
+  }, [])
 
 
   return (
@@ -103,11 +118,11 @@ export const Home: React.FC<HomeScreenProps> = (props) => {
         <Button title={"Sign out"} onPress={handleSignOut} />
         <TextInput
           placeholder="Contact to add"
-          onChangeText={text => setContact(text)}
-          value={contact}
+          onChangeText={text => setInput(text)}
+          value={input}
         />
         <Button title={"Create a chat"} onPress={handleNewChat} />
-        <Text>chats go here</Text>
+        <Text></Text>
       </View>
     </>
   );
